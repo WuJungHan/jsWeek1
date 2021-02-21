116bee88b218f919c8c54323a954729e0986a132
WuJungHan <eva29485577@gmail.com>
2021-02-21 17:04:39 +0800

    update

--- js/all.js
+++ js/all.js
@@ -1,41 +1,60 @@
 /***   作業 1 填答區開始   **/
-
-
-
+//1-1
+const janeOld = 14;
+//1-2
+const janePhoneNumber = "0955713484";
+//1-3
+const isWakeUp = false;
+//1-4
+console.log(janeOld, janePhoneNumber, isWakeUp);
 /***   作業 1 填答區結束   **/
 
 /***   作業 2 填答區開始   **/
-
-
-
+//2-1
+const steakPrice = 50;
+const fishSteakPrice = 30;
+const ricePrice = 10;
+let walletTotal = 200;
 /***   作業 2 填答區結束   **/
 
 /***   作業 3 填答區開始   **/
+//3-1 3-2 3-3
+let steakNum = 2;
+let fishSteakNum = 1;
+let riceNum = 3;
 
-
-
+let cost = 0;
+cost += (steakPrice * steakNum) + (fishSteakPrice * fishSteakNum) + (ricePrice * riceNum);
+console.log(`小明買完飯,他還剩下${walletTotal - cost}元`
+);
 /***   作業 3 填答區結束   **/
 
 /***   作業 4 填答區開始   **/
-
-
-
+//4-1
+let trafficLight = "red"
+let countdownSeconds = 35;
+let isCanPass = false;
+//4-2
+let peopleNum = 10;
+//4-3
+let cloudNum = 3;
+const sunNum = 1;
 /***   作業 4 填答區結束   **/
 
-
-
 /***   作業 5 填答區開始   **/
-
-
-
+let score = 50;
+let successfulNum = 62;
+let title = "【最終試煉】";
 /***   作業 5 填答區結束   **/
 
-
-
 /***   作業 6 填答區開始   **/
-
-
-
+let a = 1;//宣告變數a 值1
+let b = 0;//宣告變數b 值0
+a = 3;//重新賦予a值3
+a = b + 2;//重新賦予a值0+2=2
+a - b;//只是計算沒有賦予 2-2=0 目前a的值還是2
+b += 1;//重新賦予b值b+1=1
+//答案變數a值為2,變數b值為1
 /***   作業 6 填答區結束   **/
 
 /***   作業 7 填答區開始   **/
@@ -45,20 +64,43 @@
 /***   作業 7 填答區結束   **/
 
 /***   作業 8 填答區開始   **/
-
-
-
+let maryAppleNum = 20;
+//早上吃
+maryAppleNum -= 5;
+//中午吃
+maryAppleNum -= 12;
+//下午買
+maryAppleNum += 4;
+console.log(`Mary 還有 ${maryAppleNum} 顆蘋果`);
 /***   作業 8 填答區結束   **/
 
 
 /***   作業 9 填答區開始   **/
-
-
-
+//9-1
+let bill = 0;
+//9-2
+const hamburgerPrice = 30;
+//9-3
+let hamburgerNum = 3;
+//9-4
+let hamburgerBill = hamburgerPrice * hamburgerNum;
+//9-5
+const milkTeaPrice = 15;
+//9-6
+let milkTeaNum = 32;
+//9-7
+let milkTeaBill = milkTeaPrice * milkTeaNum;
+//9-8
+bill += hamburgerBill + milkTeaBill;
+//9-9
+console.log(`Nick 漢堡吃了${hamburgerBill}元,奶茶喝了${milkTeaBill}元,總共帳單是${bill}元。`)
 /***   作業 9 填答區結束   **/
 
 /***   作業 10 填答區開始   **/
-
-
-
+let g = "hello";//字串型別
+let h = 123;//數值型別
+let i = g + h;//字串型別
+let j = true;//布林值
+let k = h + h;//數值型別
+let l = j + k;//undefined
 /***   作業 10 填答區結束   **/
